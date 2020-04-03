56cd951ee1b3dec67ccc48a31db7f8d5cacc3fb1
Harsh Agrawal <harsh.ag14901@gmail.com>
2020-04-04 03:54:04 +0530

    Made changes to models and created the API base code
    properties were given to some models and all models were serialized. Now they can be viewed as JSON objects

---
+++ MainSystem/app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls import include, url
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'facilities',FacilityViewSet)
+router.register(r'wards',WardViewSet)
+router.register(r'rooms',RoomViewSet)
+router.register(r'people',PersonViewSet)
+router.register(r'groups',GroupViewSet)
+router.register(r'luxuries',LuxuryViewSet)
+urlpatterns = [
+    url('',include(router.urls)),
+    url(r'^$',index,name="index")
+]
--- MainSystem/app/views.py
+++ MainSystem/app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http.response import HttpResponse
+from rest_framework.viewsets import *
+from .models import *
+from .serializers import *
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1> Hi </h1>")
+
+class FacilityViewSet(ModelViewSet):
+    queryset = Facility.objects.all()
+    serializer_class = FacilitySerializer
+
+class WardViewSet(ModelViewSet):
+    queryset = Ward.objects.all()
+    serializer_class = WardSerializer
+
+class RoomViewSet(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class PersonViewSet(ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class GroupViewSet(ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class LuxuryViewSet(ModelViewSet):
+    queryset = Luxury.objects.all()
+    serializer_class = LuxurySerializer
