cd5977d921b455875ede883654b7472eae74c567
saptarshi <saptarshi2001@gmail.com>
2020-04-04 14:19:14 +0530

    set up react django-link

---
+++ src/reactify-ui/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Facility from './facility/Facility';
+
+class App extends Component {
+  render() {
+    return (
+      <Facility/>
+    );
+  }
+}
+
+export default App;
