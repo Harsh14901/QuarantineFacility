49e3042bda896b6de9811854065b6f50ba3be05d
Pranjal Aggarwal <pranjal2041@gmail.com>
2020-04-07 20:19:51 +0530

    Basic Form Design

---
+++ src/reactify-ui/src/assets/jss/homeStyles.js
@@ -0,0 +1,26 @@
+const style= {
+  body: {
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+  },
+  container: {
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center"
+  },
+  formDiv: {
+          width: "40%",
+          marginTop: "6vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+  },
+  textField: {
+          marginTop: "2vh"
+  }
+};
+
+export default style
