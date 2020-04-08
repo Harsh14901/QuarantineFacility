1cd11074ddd299f75ad624c0ea355ef76237090e
Pranjal <xyz>
2020-04-08 19:06:46 +0530

    Add design features

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -13,14 +13,24 @@ import People from "@material-ui/icons/People";
 import {AccountCircle} from "@material-ui/icons";
 import MenuItem from "@material-ui/core/MenuItem";
 import CustomTabs from "components/CustomTabs/CustomTabs";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import tooltipsStyle from "assets/jss/material-kit-react/tooltipsStyle";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import IconButton from "@material-ui/core/IconButton";
 
 
 
 const useStyles = makeStyles(styles);
+const tooltipUseStyles =makeStyles(tooltipsStyle);
 
 function App() {
         const classes=useStyles();
-        const [addGrp,setAddGrp] =useState(false);
+        const tooltipClasses = tooltipUseStyles();
+        const [addGrp,setAddGrp] =useState(true);
         const defaultUserDetail={name: "",age: 18,address: "",severe: false,contact: "",email: ""};
         const [userDetails,setUserDetails] = useState([defaultUserDetail,defaultUserDetail]);
 
@@ -54,6 +64,12 @@ function App() {
 
         }
 
+        function addMember() {
+                let temp=[...userDetails];
+                temp.push(defaultUserDetail);
+                setUserDetails(temp)
+        }
+
         useEffect(() => {
 
         }, []);
@@ -78,10 +94,10 @@ function App() {
                               <CustomTabs headerColor="primary" tabs={userDetails.map((data2,j) => (
                                   {
                                           tabName: "Member "+(j+1),
-                                          tabContent: (<div>
-                                      <TextField className={classes.textField} fullWidth={true} onChange={handleChange('name')} color="#ff00ff" label="Name" required defaultValue={userDetails[0].name}
+                                          tabContent: (<div style={{display: "flex",justifyContent: "center",flexDirection: "column",alignItems:"center"}}>
+                                      <TextField className={classes.textField} fullWidth={true} onChange={handleChange('name')} color="primary" label="Name" required defaultValue={userDetails[j].name}
                                                  InputProps={{
-                                                         style: {fontSize: "1.0rem"},
+                                                         style: {fontSize: "0.9rem"},
                                                          endAdornment: (
                                                              <InputAdornment position="end">
                                                                      <People />
@@ -89,10 +105,11 @@ function App() {
                                                          ),
                                                  }}>
                                       </TextField>
-                                        <TextField className={classes.textField} fullWidth={true} onChange={handleChange('age')} color="secondary" type="number" label="Age" required value={userDetails[0].age}>
+                                        <TextField className={classes.textField} fullWidth={true} onChange={handleChange('age')} color="primary" type="number" label="Age" required value={userDetails[j].age}>
                                         </TextField>
-                                      <TextField className={classes.textField} fullWidth={true} onChange={handleChange('address')} color="secondary" label="Address" required defaultValue={userDetails[0].address}
+                                      <TextField className={classes.textField} fullWidth={true} color="primary" onChange={handleChange('address')} label="Address" required defaultValue={userDetails[j].address}
                                                  InputProps={{
+                                                         style: {fontSize: "0.9rem"},
                                                          endAdornment: (
                                                              <InputAdornment position="end">
                                                                      <People />
@@ -106,18 +123,22 @@ function App() {
                                   id="severity"
                                   select
                                   label="Severity"
-                                  value={userDetails[0].severe}
+                                  value={userDetails[j].severe}
                                   onChange={handleChange('severe')}
                                   fullWidth={true}
+                                  InputProps={{style: {fontSize: "0.9rem"}}}
+
+
                               >
                                       {severeCateg.map((option) => (
-                                          <MenuItem key={option.value} value={option.value}>
+                                          <MenuItem  key={option.value} value={option.value}>
                                                   {option.label}
                                           </MenuItem>
                                       ))}
                               </TextField>
-                              <TextField className={classes.textField} fullWidth={true} onChange={handleChange('contact')} type="number" color="secondary" label="Contact Number" required defaultValue={userDetails[0].contact}
+                              <TextField className={classes.textField} fullWidth={true} onChange={handleChange('contact')} type="number" color="primary" label="Contact Number" required defaultValue={userDetails[j].contact}
                                          InputProps={{
+                                                 style: {fontSize: "0.9rem"},
                                                  endAdornment: (
                                                      <InputAdornment position="end">
                                                              <People />
@@ -126,9 +147,10 @@ function App() {
                                          }}>
 
                               </TextField>
-                                      <TextField className={classes.textField} fullWidth={true} onChange={handleChange('email')}  color="secondary" label="Email" required defaultValue={userDetails[0].email}
+                                      <TextField className={classes.textField} fullWidth={true} onChange={handleChange('email')}  color="primary" label="Email" required defaultValue={userDetails[j].email}
 
                                                  InputProps={{
+                                                         style: {fontSize: "0.9rem"},
                                                          endAdornment: (
                                                              <InputAdornment position="end">
                                                                      <Email />
@@ -137,10 +159,34 @@ function App() {
                                                  }}>
 
                                       </TextField>
-
-                              <button onClick={submitDetails}>
+                              <div style={{position:"relative",width:"100%"}}>
+                              <Button onClick={submitDetails} className={classes.submitButton}>
                                       Submit
-                              </button>
+                              </Button>
+                                      <Tooltip
+                                          id="tooltip-top"
+                                          title="Add a member"
+                                          placement="top"
+                                          classes={{ tooltip: tooltipClasses.tooltip }}
+                                      >
+                                      <Fab onClick={addMember} style={{right: "5px",top: "20%",position:"absolute"}} size="small" color="primary" aria-label="add">
+                                              <AddIcon/>
+
+                                      </Fab>
+                                      </Tooltip>
+                                      <Tooltip
+                                          id="tooltip-top"
+                                          title="Delete this member"
+                                          placement="top"
+                                          classes={{ tooltip: tooltipClasses.tooltip }}
+                                      >
+                                              <Fab onClick={addMember} style={{right: "55px",top: "20%",position:"absolute"}} size="small" color="primary" aria-label="add">
+                                                      <DeleteIcon/>
+
+                                              </Fab>
+                                      </Tooltip>
+
+                              </div>
                                               </div>
 
                                       )}))}>
