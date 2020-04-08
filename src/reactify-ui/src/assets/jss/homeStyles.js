e5967b1631d1c2579e02baa583e24e82717c8330
Pranjal Aggarwal <pranjal2041@gmail.com>
2020-04-08 19:06:46 +0530

    Add design features

--- src/reactify-ui/src/assets/jss/homeStyles.js
+++ src/reactify-ui/src/assets/jss/homeStyles.js
@@ -19,7 +19,21 @@ const style= {
           justifyContent: "center"
   },
   textField: {
-          marginTop: "2vh"
+          marginTop: "2vh",
+          fontSize: "0.4rem",
+          marginRight: "20px",
+          marginLeft: "20px",
+  },
+  submitButton: {
+          background: 'linear-gradient(45deg, #A742B9 30%, #9026AB 90%)',
+          border: 0,
+          borderRadius: 3,
+          boxShadow: '0 3px 5px 2px rgba(167, 66, 185, .3)',
+          color: 'white',
+          padding: '0 30px',
+          marginTop: "2vh",
+          left: "30%",
+          position: "relative"
   }
 };
 
