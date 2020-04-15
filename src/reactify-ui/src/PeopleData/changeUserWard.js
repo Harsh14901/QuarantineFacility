9a8f0e2ce6cc603792df9df5b51b5623518861ff
Pranjal <xyz>
2020-04-15 16:26:43 +0530

    Improve user details UI

--- src/reactify-ui/src/PeopleData/changeUserWard.js
+++ src/reactify-ui/src/PeopleData/changeUserWard.js
@@ -2,19 +2,19 @@ import cookie from "react-cookies";
 import axios from 'axios';
 import {func} from "prop-types";
 
-const PostGroupData = (callback,data) => {
+const changeUserWard = (callback,data) => {
 
         const endpt = 'http://127.0.0.1:8000/allocate/';
         let lookupOpts = {
-                method:'POST',
+                method:'PATCH',
                 headers:{
                         'Content-Type':'application/json',
                         'Authorization': 'Basic YWRtaW46YWRtaW4xMjM0',
                         'Accept': 'application/json'
                 }
         };
-        axios.post(endpt, data,lookupOpts). then(r => callback(data)).catch(function(error){console.log("hi",error)})
+        axios.patch(endpt, data,lookupOpts). then(r => callback(data)).catch(function(error){console.log("hi",error)})
 
 };
 
-export default PostGroupData
+export default changeUserWard
--- src/reactify-ui/src/views/UserProfile/UserDetails.js
+++ src/reactify-ui/src/views/UserProfile/UserDetails.js
@@ -15,17 +15,58 @@ import IconButton from "@material-ui/core/IconButton";
 import {Email, Phone} from "@material-ui/icons";
 import HighRiskIcon from "assets/img/icons/HighRiskIcon";
 import VipIcon from "assets/img/icons/VipIcon";
+import './userDetails.css'
+import Dialog from "@material-ui/core/Dialog";
+import Transition from "react-transition-group/Transition";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import {func} from "prop-types";
+import changeUserWard from "PeopleData/changeUserWard";
+import Zoom from "@material-ui/core/Zoom";
+import Tooltip from "@material-ui/core/Tooltip";
+import TootltipStyles from 'assets/jss/material-kit-react/tooltipsStyle'
 
 const useStyles2 = makeStyles(styles2);
 const useStyles = makeStyles(styles);
+const useStyles3 = makeStyles(TootltipStyles);
 
+const Para = ({text}) => {
+        return(
+        <p style={{fontSize:"1rem",fontWeight: "300"}}>{text}</p>
+        )
+};
+
+const SideButton = (props) => {
+        return(
+            <Button onClick={props.onClick} size="small" variant="outlined" color="primary" style={{marginLeft: "auto",fontSize: "0.7rem"}}>{props.text}</Button>
+        )
+};
 
 export default function UserDetails(props) {
 
         const classes2 = useStyles2();
         const classes = useStyles();
+        const classes3 = useStyles3();
 
         const [cardAnimation,setCardAnimation] =useState("cardHidden");
+        const [open,setOpen] = useState(false);
+
+        function handleClose() {
+                setOpen(false)
+        }
+        function handleWardChange() {
+                setOpen(true);
+        }
+
+        function changeWard() {
+                const callback = result => {
+                        console.log("Seems to have changed ward",result)
+                };
+
+                changeUserWard(callback,{id:props.data.id,risk: props.data.risk})
+        }
 
         function dischargeUser() {
 
@@ -54,43 +95,59 @@ export default function UserDetails(props) {
                                     <div>
                                             <GridContainer>
                                                     <GridItem xs={12}>
-                                                            <p>{"User id:- "+props.data.id}</p>
+                                                            <Para text={"User id:- " + props.data.id}/>
                                                     </GridItem>
                                                     <GridItem xs={6}>
-                                                            <p>{"Age:- "+props.data.age}</p>
+                                                            <Para text={"Age:- " + props.data.age}/>
                                                     </GridItem>
                                                     <GridItem xs={6}>
-                                                            {(props.data.gender!=='other')?<p>{"Gender:- "+props.data.gender}</p>:null}
+                                                            {(props.data.gender!=='other')?<Para text={"Gender:- " + props.data.gender}/>:null}
                                                     </GridItem>
                                             </GridContainer>
                                             <div style={{display: "flex"}}>
-                                                    <p>{"Ward Category:- "+props.data.ward}</p>
-                                                    <Button variant="outlined" color="primary" style={{marginLeft: "auto",fontSize: "0.7rem"}}>{"Change Ward to "+(props.data.ward==='1')?2:1}</Button>
+                                                    <Para text={"Ward Category:- " + ((props.data.risk) ? 1 : 2)}/>
+                                                    <SideButton onClick={handleWardChange} text={"Shift to Ward "+(props.data.risk?2:1)}/>
                                             </div>
                                             <div style={{display: "flex",marginTop:"10px"}}>
-                                                    <p>{"Facility Enrolled:- "+props.data.facility}</p>
+                                                    <Para text={"Facility Enrolled:- " + props.data.facility}/>
                                                     <Button size="small" variant="outlined" color="primary" style={{marginLeft: "auto",fontSize: "0.7rem"}}>Change Facility</Button>
                                             </div>
                                             <div style={{display: "flex",marginTop:"10px"}}>
-                                                    <p>{"Group ID:- "+props.data.facility}</p>
+                                                    <Para text={"Group ID:- " + props.data.facility}/>
                                                     <Button size="small" variant="outlined" color="primary" style={{marginLeft: "auto",fontSize: "0.7rem"}}>View Group Details</Button>
                                             </div>
-                                            <p>{"Address:- "+props.data.address}</p>
-                                            <p>{"Date of Admission:- "+ props.data.date}</p>
+                                            <Para text={"Address:- " + props.data.address}/>
+                                            <Para text={"Date of Admission:- " + props.data.date}/>
                                     </div>
                                             {props.data.number?
                                             <div style={{display: "flex",marginTop:"10px"}}>
-                                            <p>{"Contact Number:-\t"+props.data.number}</p>
-                                            <IconButton style={{marginLeft: "auto"}} color="primary" aria-label="Contact" component="span">
-                                                    <Phone />
-                                            </IconButton>
+                                            <Para style={{fontSize: "1rem"}} text={"Contact Number:-\t" + props.data.number}/>
+                                                    <a href={"tel:+91"+props.data.number} target="_blank" style={{marginLeft: "auto",marginTop:"10px"}}>
+                                                            <Tooltip
+                                                                id="tooltip-bottom"
+                                                                title={<p style={{fontSize: "14px"}}>{"Contact: +91 " +props.data.number}</p>}
+                                                                placement="bottom"
+                                                                TransitionComponent={Zoom}
+                                                                classes={{ tooltip: classes3.tooltip }}
+                                                            >
+                                                                    <Phone color="primary" />
+                                                            </Tooltip>
+                                                    </a>
                                             </div>:null}
                                             {props.data.email?
                                                 <div style={{display: "flex",marginTop:"10px"}}>
-                                                <p>{"Email:-\t"+props.data.email}</p>
-                                            <IconButton style={{marginLeft: "auto"}} color="primary" aria-label="Contact" component="span">
-                                                    <Email />
-                                            </IconButton>
+                                                <Para text={"Email:-\t" + props.data.email}/>
+                                                        <a href={"mailto:"+props.data.email} target="_blank" style={{marginLeft: "auto",marginTop:"10px"}}>
+                                                                <Tooltip
+                                                                    id="tooltip-bottom"
+                                                                    title={<p style={{fontSize: "14px"}}>{props.data.email}</p>}
+                                                                    placement="bottom"
+                                                                    TransitionComponent={Zoom}
+                                                                    classes={{ tooltip: classes3.tooltip }}
+                                                                >
+                                                                        <Email color="primary" />
+                                                                </Tooltip>
+                                                        </a>
                                                 </div>
                                                 :null}
 
@@ -106,6 +163,33 @@ export default function UserDetails(props) {
                             </CardFooter>
                     </form>
             </Card>
+
+
+                    <Dialog
+                        open={open}
+                        TransitionComponent={Transition}
+                        keepMounted
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-slide-title"
+                        aria-describedby="alert-dialog-slide-description"
+                    >
+                            <DialogTitle id="alert-dialog-slide-title">{"Change User's Ward?"}</DialogTitle>
+                            <DialogContent>
+                                    <DialogContentText id="alert-dialog-slide-description">
+                                            {"Are you sure you want to change "+props.data.name+"'s ward?"}
+                                    </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                    <Button onClick={handleClose} color="primary">
+                                            Cancel
+                                    </Button>
+                                    <Button onClick={changeWard} color="primary">
+                                            Yes, Change Ward
+                                    </Button>
+                            </DialogActions>
+                    </Dialog>
+
+
             </div>
         )
 };
