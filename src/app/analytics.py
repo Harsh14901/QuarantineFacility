67294fd999bc7d054cede626c45ae5459b104266
saptarshi1234 <2001saptarshi@gmail.com>
2020-04-16 11:16:15 +0530

    add ward_distribution, active_cases ,and send info on all days(even when no cases on that day

--- src/app/analytics.py
+++ src/app/analytics.py
@@ -2,9 +2,42 @@ from .models import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.db.models import Count,Sum
-
+from datetime import *
 
 DATE_FORMAT = '%d/%m/%y'
+start_date = datetime(2020,4,1)
+
+
+def add_days(coordinates):                                  #fill in values of new_cases
+    curr_date = start_date
+    while curr_date < datetime.now():
+        if not curr_date.strftime(DATE_FORMAT) in coordinates.keys():
+            print(curr_date)
+            coordinates[curr_date.strftime(DATE_FORMAT)] = 0
+        curr_date += timedelta(days=1)
+    
+    # sort
+    ans = dict()
+    for key in sorted(coordinates):
+        ans[key] = coordinates[key]
+    return ans
+
+def add_days_special(coordinates):                        # fill in values of total_cases
+    curr_date = start_date
+    last=0
+    while curr_date < datetime.now():
+        if not curr_date.strftime(DATE_FORMAT) in coordinates.keys():
+            coordinates[curr_date.strftime(DATE_FORMAT)] = last
+        else:
+            last = coordinates[curr_date.strftime(DATE_FORMAT)]
+        curr_date += timedelta(days=1)
+
+    # sort
+    ans = dict()
+    for key in sorted(coordinates):
+        ans[key] = coordinates[key]
+    return ans
+    
 
 @api_view(['GET'])
 def discharge_count(request):
@@ -12,7 +45,7 @@ def discharge_count(request):
     queryset = Discharged.objects.values('date_discharged').annotate(Count('person'))
     for point in queryset:
         coordinates[point['date_discharged'].strftime(DATE_FORMAT)] = point['person__count']
-    return Response(coordinates)
+    return Response(add_days(coordinates))
 
 @api_view(['GET'])
 def avg_discharge_time(request):
@@ -29,8 +62,7 @@ def avg_discharge_time(request):
                 total_discharge_time += obj.person.discharge_time
                 count += 1
             coordinates[x] = total_discharge_time.days/count
-        
-    return Response(coordinates)
+    return Response(add_days(coordinates))
             
 
 @api_view(['GET'])
@@ -39,7 +71,8 @@ def new_cases(request=None):
     queryset = Person.objects.values('doa').annotate(Count('doa'))
     for point in queryset:
         coordinates[point['doa'].strftime(DATE_FORMAT)] = point['doa__count']
-    return Response(coordinates)
+    
+    return Response(add_days(coordinates))
 
 
 @api_view(['GET'])
@@ -52,7 +85,17 @@ def total_cases(request):
         if x not in coordinates.keys():
             count += Person.objects.filter(doa=doa).count()
             coordinates[x] = count
-    return Response(coordinates)
+    return Response(add_days_special(coordinates))
+
+@api_view(['GET'])
+def active_cases(request):
+    print(request)
+    coordinates = {}
+    curr_date = start_date
+    total = total_cases(request=request._request).data
+    discharged = total_discharges(request=request._request).data
+    ans={}
+    return(Response({day : total[day] - discharged[day] for day in total.keys()}))
 
 @api_view(['GET'])
 def cases_vs_age(request):
@@ -81,4 +124,15 @@ def total_discharges(request):
         if x not in coordinates.keys():
             count += Discharged.objects.filter(date_discharged=dod).count()
             coordinates[x] = count
-    return Response(coordinates)
+    return Response(add_days_special(coordinates))
+
+@api_view(['GET'])
+def get_ward_distribution(request):
+    distribution = {'Ward-1':0,'Ward-2':0,'vacant':0}
+    for room in Room.objects.all():
+        if room.ward.category == Ward.WARD1:
+            distribution['Ward-1'] += room.occupant_count
+        else:
+            distribution['Ward-2'] += room.occupant_count
+        distribution['vacant'] += room.capacity - room.occupant_count
+    return Response(distribution)
