abd9385f236f9528fa49efc2fd5826a1b7e209b0
Harsh Agrawal <harsh.ag14901@gmail.com>
2020-04-17 22:51:01 +0530

    Minor Bug Fix

--- src/MainSystem/settings.py
+++ src/MainSystem/settings.py
@@ -143,9 +143,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 REST_FRAMEWORK = {
-	# 'DEFAULT_PERMISSION_CLASSES': (
-	# 	'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-	# ),
+	'DEFAULT_PERMISSION_CLASSES': (
+		'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+	),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
