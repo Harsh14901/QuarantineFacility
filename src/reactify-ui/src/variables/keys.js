dfdc4b326d65f8c6e3f59f9a5e825bae143da8b4
Pranjal Aggarwal <pranjal2041@gmail.com>
2020-05-11 14:30:44 +0530

    clean files

--- src/reactify-ui/src/variables/keys.js
+++
@@ -1,9 +0,0 @@
-export const GroupDataKey = 'group_data';
-
-export const UserDataKey = 'user_data';
-
-export const FacilityDataKey = 'facility_data';
-
-export const luxuryDataKey = 'luxury_data';
-
-export const medicineDataKey = 'medicine_data';
